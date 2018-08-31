3e2c4c0b07f1af7f6e876eb3571b735a2435d791
raman tehlan <ramantehlan@gmail.com>
2018-08-31 23:40:14 +0530

    All APIs structured

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,58 +18,47 @@ export class HomePage {
     * Variables
     **********************/
 
+    // Default Values 
+    openWeatherMapKey: string = "b48d71c76bc12ce4415b45cbb858195f";
+    breezoMeterKey: string = "2d2a717abed743c982bf247bb59be090";
+
+    lat: string = "28.704060";
+    lon: string = "77.102493";
+
     // Date 
+    airData: object;
     uvData: object;
-    test:string = "Test"
+    weatherData: object;
+    
+    // API Links
+    airApi: string = "https://api.breezometer.com/baqi/";
+    uvApi: string = "http://api.openweathermap.org/data/2.5/uvi";
+    weatherApi: string = "http://api.openweathermap.org/data/2.5/weather";
 
-    /*
-	Constant values
-    */
-
-    // weather
-    metaWeather: string = "https://www.metaweather.com/api/location/search/";
-    mwPara: object = {
-    	"lattlong": "28.704060,77.102493"
-    }
-    mwHeader: object= {
-        'Content-Type': 'application/json; charset=utf-8',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Origin': 'https://www.metaweather.com',
-        'Access-Control-Allow-Methods': 'GET,POST',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        "Authorization": "Negotiate"
-    }
-
-    // Url from where the data is to be fetched
-    air: string = "https://api.breezometer.com/baqi/"
-    uv: string = "http://api.openweathermap.org/data/2.5/uvi";
-    weather: string = "http://api.openweathermap.org/data/2.5/weather?appid=b48d71c76bc12ce4415b45cbb858195f&lat=28.704060&lon=77.102493&callback=JSONP_CALLBACK";
-    // Parameters object for login api
-    uvPara:object = {
-    	"appid": "b48d71c76bc12ce4415b45cbb858195f",
-    	"lat": "28.704060",
-    	"lon": "77.102493",
-    }
-    // Para for air
-    para2: object ={
+    // Parameters 
+    airPara:object = {
     	"appid": "2d2a717abed743c982bf247bb59be090",
     	"lat": "28.632430",
     	"lon": "77.218790",
-    	"callback": "JSONP_CALLBACK"
     }
-    // Header object for login api
-    uvHeader:object =  {
+    uvPara:object = {
+    	"appid": "2ffcf64c74128a6644f33e554d86766c",
+    	"lat": "28.632430",
+    	"lon": "77.218790",
+    }
+    weatherPara:object = {
+    	"appid": "2ffcf64c74128a6644f33e554d86766c",
+    	"lat": "28.632430",
+    	"lon": "77.218790",
+    }
 
+    // Header object for login api
+    header:object =  {
         'Content-Type': 'application/json; charset=utf-8',
-         /*
-        'x-api-key': 'b48d71c76bc12ce4415b45cbb858195f',
-        'X-Cache-Key': '/data/2.5/uviweather?appid=b48d71c76bc12ce4415b45cbb858195f&lat=28.704060&lon=77.102493',
         'Access-Control-Allow-Credentials': 'true',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,POST',
         'Access-Control-Allow-Headers': 'Content-Type'
-        */
-
     }
 
     /*********************
@@ -81,8 +70,16 @@ export class HomePage {
     *    This function should start after setDAta
     *
     */
-    getUV(): Observable<any>{
-            return this.HttpService.fetch(this.uv, this.uvHeader, this.uvPara, "UV");
+    getAir(): Observable<any>{
+            return this.HttpService.fetch(this.airApi, this.header, this.airPara, "UV");
+    }
+
+    getUv(): Observable<any>{
+            return this.HttpService.fetch(this.uvApi, this.header, this.uvPara, "UV");
+    }
+
+    getWeather(): Observable<any>{
+            return this.HttpService.fetch(this.weatherApi, this.header, this.weatherPara, "UV");
     }
 
 
@@ -105,9 +102,25 @@ export class HomePage {
 
   	// To get the UV Data
   	// This is to make a http call to fetch user information
-    this.getUV().subscribe(
+    this.getAir().subscribe(
+        data => {
+          this.airData = data;
+          console.log(data);
+        }, 
+        error => alert("error")
+      );
+
+    this.getUv().subscribe(
         data => {
           this.uvData = data;
+          console.log(data);
+        }, 
+        error => alert("error")
+      );
+
+    this.getWeather().subscribe(
+        data => {
+          this.weatherData = data;
           console.log(data);
         }, 
         error => alert("error")
